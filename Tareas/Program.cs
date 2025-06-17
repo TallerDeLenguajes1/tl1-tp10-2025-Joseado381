bf9eb4def9f9b13dd1a5dc48e6cb1de195f88adc
Joseado381 <josealonzo.jaa@gmail.com>
2025-06-17 13:29:14 -0300

    primer commit del tp

---
+++ Tareas/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+namespace tareas
+{
+    class tareas
+    {
+        //private static HttpClient 
+    }
+}
