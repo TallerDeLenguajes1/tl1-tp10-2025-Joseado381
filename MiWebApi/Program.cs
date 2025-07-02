10a442e9540d13f367abb3c30487aee2fe5ad126
Joseado381 <josealonzo.jaa@gmail.com>
2025-07-02 12:21:21 -0300

    Ses me olvido de pushear los ultimos tps

---
+++ MiWebApi/Program.cs
@@ -0,0 +1,52 @@
+﻿using classDeudor;
+using System;
+using System.Text.Json;
+
+bool Bandera = true;
+List<Deudor> listDeudor = new List<Deudor>();
+List<string> infoDeudor = new List<string>();
+do
+{
+
+    Console.WriteLine("Ingrese su numero de Cuil");
+    string cuil = Console.ReadLine() ?? "";
+    HttpClient client = new HttpClient();
+    HttpResponseMessage response = await client.GetAsync($"https://api.bcra.gob.ar/centraldedeudores/v1.0/Deudas/{cuil}");
+    response.EnsureSuccessStatusCode();
+    string responseBody = await response.Content.ReadAsStringAsync();
+    Deudor deudor = JsonSerializer.Deserialize<Deudor>(responseBody);
+    Console.WriteLine("\n-------------------------");
+    Console.WriteLine("\nDatos del Deudor");
+    Console.WriteLine($"\nNombre : {deudor.Results.Denominacion}\n Cuil: {deudor.Results.Identificacion}");
+    foreach (var periodos in deudor.Results.Periodos)
+    {
+        DateTime fecha = DateTime.ParseExact(periodos.Periodo, "yyyyMM", null);
+        string formato = fecha.ToString("yy-MM");
+        Console.WriteLine($"\n Ultima Actualizacion de la Deuda {formato}");
+        foreach (var bancos in periodos.Entidades)
+        {
+
+            Console.WriteLine($"\n Entidad : {bancos.EntidadBancaria} \n Situacion :{bancos.Situacion}   Monto : ${bancos.Monto}mil  \n Etapa judicial : {(bancos.ProcesoJud ? "En juicio" : "Sin juicio")}");
+
+            infoDeudor.Add($" Entidad : {bancos.EntidadBancaria}- Situacion :{bancos.Situacion}   Monto : ${bancos.Monto}mil - Etapa judicial : {(bancos.ProcesoJud ? "En juicio" : "Sin juicio")}");
+        }
+    }
+    Console.WriteLine("\n-------------------------");
+    listDeudor.Add(deudor);
+    Console.WriteLine("Desea Parar (y/n)");
+    string pararPrograma = Console.ReadLine() ?? "";
+    if (pararPrograma == "y" || pararPrograma != "n")
+    {
+        Bandera = false;
+    }
+
+} while (Bandera);
+
+DateTime FechaHoy = DateTime.Today;
+string fechacsv = FechaHoy.ToString("yyyyMMdd__HHmmss");
+string jsonStringCsv = JsonSerializer.Serialize<List<string>>(infoDeudor);
+string jsonString = JsonSerializer.Serialize<List<Deudor>>(listDeudor);
+File.WriteAllText($"deudores{fechacsv}.txt", jsonStringCsv);
+File.WriteAllText("deudores.json", jsonString);
+
+
--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,10 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.Json;
+using tarea;
 
-namespace tareas
+
+HttpClient client = new HttpClient();
+HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/");
+response.EnsureSuccessStatusCode();
+string? responseBody = await response.Content.ReadAsStringAsync();
+List<Tarea>? listTarea = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+Console.WriteLine("Tareas completadas:\n");
+foreach (var Prov in listTarea)
 {
-    class tareas
+
+    if (Prov.Estado == true)
     {
-        //private static HttpClient 
+        Console.WriteLine($"userID{Prov.UserId}, Id:{Prov.Id}, title :{Prov.Title}, estado : {Prov.Estado}");
     }
 }
+Console.WriteLine("Tareas pendientes: \n");
+
+foreach (var Prov in listTarea)
+{
+
+    if (Prov.Estado != true)
+    {
+
+        Console.WriteLine($"userID{Prov.UserId}, Id:{Prov.Id}, title :{Prov.Title}, estado : {Prov.Estado}");
+    }
+}
+
+string jsonString = JsonSerializer.Serialize<List<Tarea>>(listTarea);
+
+File.WriteAllText("tareas.json", jsonString);
+
+
+
+
+
+
